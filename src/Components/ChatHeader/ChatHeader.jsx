e154a546ebe9217d3da9291bb00b8ce82af67e3d
alan barbera <aedev86@gmail.com>
2026-03-07 00:32:26 -0300

    edit: rem & 4px

--- src/Components/ChatHeader/ChatHeader.jsx
+++ src/Components/ChatHeader/ChatHeader.jsx
@@ -1,8 +1,18 @@
 import './ChatHeader.css'
+import { useNavigate } from 'react-router';
+import useWindowSize from '../../hooks/useWindowSize';
 
 export default function ChatHeader({ contact_selected }) {
+  const isMobile = useWindowSize();
+  const navigate = useNavigate();
+
   return (
     <div className="chatHeader-container">
+      {isMobile && (
+        <button className='backBtn' onClick={() => navigate('/')}>
+          ←
+        </button>
+      )}
       <img src={contact_selected.profile_picture} alt={contact_selected.name} />
       <div className='chatHeader-infoContact'>
         <h3>{contact_selected.name}</h3>
--- src/Components/MainLayout/MainLayout.jsx
+++ src/Components/MainLayout/MainLayout.jsx
@@ -1,23 +1,35 @@
-import { Outlet } from "react-router";
-import ContactSidebar from "../ContactSidebar/ContactSidebar";
-import SideNav from "../SideNav/SideNav";
 import "./MainLayout.css";
+import { useContext } from "react";
+import { Outlet } from "react-router";
+import SideNav from "../SideNav/SideNav";
+import useWindowSize from "../../hooks/useWindowSize";
+import ContactSidebar from "../ContactSidebar/ContactSidebar";
+import { ContactsContext } from "../../Context/ContactsContext";
 
 export default function MainLayout() {
+  const isMobile = useWindowSize();
+  const { contact_selected } = useContext(ContactsContext);
+
   return (
     <div className="asideMain-container">
-      <aside className="aside-navBtn">
-        <SideNav />
-      </aside>
+      {(isMobile || !contact_selected ) && (
+        <>
+          <aside className="aside-navBtn">
+            <SideNav />
+          </aside>
 
-      <aside className="aside-contacts">
-        <ContactSidebar />
-        <div className="line-bottom"></div>
-      </aside>
+          <aside className="aside-contacts">
+            <ContactSidebar />
+            <div className="line-bottom"></div>
+          </aside>  
+        </>
+      )}
 
-      <main className="main">
-        <Outlet />
-      </main>
+      {(!isMobile || contact_selected) && (
+        <main className="main">
+          <Outlet />
+        </main>
+      )}
     </div>
   );
 }
